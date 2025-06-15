06e11dbcab215b9c90bf1930c7ed25f60c98f946
PriDev07 <lohaniprince72@gmail.com>
2025-06-15 20:05:48 +0530

    password-hashing-logic-added

--- projects/blog-api/models/user.js
+++ projects/blog-api/models/user.js
@@ -1,3 +1,4 @@
+const {createHmac,randomBytes} = require('crypto');
 const {Schema, Model} = require("mongoose");
 
 const userSchema = new Schema({
@@ -27,6 +28,17 @@ const userSchema = new Schema({
         default: "USER",
     }
 },{timestamps: true});
+
+userSchema.pre('save',function (next){
+    const user = this;
+
+    if(!user.isModified("password")) return ;
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac('sha256',salt).update(user.password).digest("hex");
+    this.salt = salt;
+    this.password = hashedPassword;
+    next();
+});
 
 const User = Model('user',userSchema);
 module.exports() = User;
